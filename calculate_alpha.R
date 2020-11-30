b968ff08758cce797ea994c9247d34faacb41621
Strumke <inga@strumke.com>
2020-11-30 14:26:15 +0100

    with check.keys (negative correlation fix)

--- calculate_alpha.R
+++ calculate_alpha.R
@@ -7,18 +7,18 @@ alpha(relevant_data) #yields raw_alpha = 0.44 - not spectacular.
 
 # Analyse questions separately
 q1 <- relevant_data[1:6]
-alpha(q1) # raw_alpha = 0.36 + warnings regarding negative correlation
+alpha(q1) # raw_alpha = 0.36 + warnings. with check.keys: 0.82
 q2 <- relevant_data[7:12]
-alpha(q2) # raw_alpha = 0.16 + warnings regarding negative correlation
+alpha(q2) # raw_alpha = 0.16 + warnings. with check.keys: 0.76
 q3 <- relevant_data[13:18]
-alpha(q3) # raw_alpha = -3.7 + warnings regarding negative correlation
+alpha(q3) # raw_alpha = -3.7 + warnings. with check.keys: 0.57
 q4 = relevant_data[19:24]
-alpha(q4) # raw_alpha = -1.3 + warnings regarding negative correlation
+alpha(q4) # raw_alpha = -1.3 + warnings. with check.keys: 0.9
 q5 <- relevant_data[25:30]
-alpha(q5) # raw_alpha = -2.2 + warnings regarding negative correlation
+alpha(q5) # raw_alpha = -2.2 + warnings. with check.keys: 0.83
 q6 <- relevant_data[31:36]
-alpha(q6) # raw_alpha = 0.17 + warnings regarding negative correlation
+alpha(q6) # raw_alpha = 0.17 + warnings. with check.keys: 0.86
 q7 <- relevant_data[37:42]
-alpha(q7) # raw_alpha = -6.5 + warnings regarding negative correlation
+alpha(q7) # raw_alpha = -6.5 + warnings. with check.keys: 0.81
 q8 <- relevant_data[43:48]
-alpha(q8) # raw_alpha = -4.1 + warnings regarding negative correlation
+alpha(q8) # raw_alpha = -4.1 + warnings. with check.keys: 0.93
